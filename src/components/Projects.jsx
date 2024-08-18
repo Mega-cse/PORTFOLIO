8811288182f2bdda80565fec987efe2ba36f8ca2
megavardhini1958@gmail.com <megavardhini1958@gmail.com>
2024-08-18 20:18:10 +0530

    change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Projects.css';
+
 const Projects = () => {
   return (
     <section id="projects">
@@ -6,43 +8,53 @@ const Projects = () => {
         <div className="main-text" id="serv">
           <h2>My <span>Projects</span></h2>
           <div className="serv-container">
-            <div className="row">
-              <img src="./img/img1.jpg" alt="E-commerce" />
-              <div className="layer">
-                <h5>E-commerce</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minima, nemo eaque assumenda accusamus odio nisi corporis totam? Eveniet voluptatibus aspernatur saepe quia odio, magni consectetur eaque repudiandae atque nobis.</p>
-                <a href="#"><i className='bx bxs-face-mask' style={{ color: 'aliceblue' }}></i></a> 
+            {projects.map((project, index) => (
+              <div className="card" key={index}>
+                <img src={project.image} alt={project.title} className="card-img" />
+                <div className="card-overlay">
+                  <h5 className="card-title">{project.title}</h5>
+                  <p className="card-description">{project.description}</p>
+                  <div className="card-links">
+                    <a href={project.github} className="card-link" target="_blank" rel="noopener noreferrer">
+                      <i className='bx bxl-github'></i> GitHub
+                    </a>
+                    <a href={project.netlify} className="card-link" target="_blank" rel="noopener noreferrer">
+                      <i className='bx bx-link'></i> Live Demo
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-            {/* <div className="row">
-              <img src="./img/DD.png" alt="Drowsiness Detection" />
-              <div className="layer">
-                <h5>Loreum</h5>
-               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit porro deleniti sint aliquid eligendi ex soluta quia consequatur, facere non, fugiat cupiditate distinctio saepe recusandae pariatur iusto illum voluptatum sequi?</p>
-                <a href="#"><i className='bx bxs-low-vision' style={{ color: 'aliceblue' }}></i></a>
-              </div>
-            </div>
-            <div className="row">
-              <img src="./img/DA.png" alt="Desktop Application" />
-              <div className="layer">
-                <h5>Loreum</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptas autem deleniti. Labore pariatur sit culpa obcaecati, ut cumque, impedit odio sint nam soluta minima recusandae libero magnam delectus similique.</p>
-                <a href="#"><i className='bx bx-desktop' style={{ color: 'aliceblue' }}></i></a>
-              </div>
-            </div>
-            <div className="row">
-              <img src="./img/DA.png" alt="Desktop Application" />
-              <div className="layer">
-                <h5>Loreum</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptas autem deleniti. Labore pariatur sit culpa obcaecati, ut cumque, impedit odio sint nam soluta minima recusandae libero magnam delectus similique.</p>
-                <a href="#"><i className='bx bx-desktop' style={{ color: 'aliceblue' }}></i></a>
-              </div>
-            </div> */}
+            ))}
           </div>
         </div>
       </div>
     </section>
   );
 };
+
+// Sample project data
+const projects = [
+  {
+    title: 'E-commerce',
+    image: '/img/img1.jpg', // Adjusted path
+    description: 'A fully-featured eCommerce website built with React, designed to provide an engaging shopping experience. This project includes components for product listing, cart management, user registration, and a secure checkout process. It integrates responsive design to ensure compatibility across various devices and screen sizes.',
+    github: 'https://github.com/Mega-cse/E-commerce-project.git',
+    netlify: 'https://stately-gumdrop-949f2b.netlify.app/'
+  },
+  {
+    title: 'Loreum',
+    image: '/img/white.jpg', // Adjusted path
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit porro deleniti sint aliquid eligendi ex soluta quia consequatur, facere non, fugiat cupiditate distinctio saepe recusandae pariatur iusto illum voluptatum sequi?',
+    github: 'https://github.com/yourusername/drowsiness-detection',
+    netlify: 'https://yourproject.netlify.app'
+  },
+  {
+    title: 'Loreum',
+    image: '/img/white.jpg', // Adjusted path
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptas autem deleniti. Labore pariatur sit culpa obcaecati, ut cumque, impedit odio sint nam soluta minima recusandae libero magnam delectus similique.',
+    github: 'https://github.com/yourusername/desktop-application',
+    netlify: 'https://yourproject.netlify.app'
+  }
+];
 
 export default Projects;
